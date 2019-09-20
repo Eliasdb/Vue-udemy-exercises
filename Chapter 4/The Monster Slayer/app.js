28651c5bc6e63335b84ac93fc51b065f7168f4d3
Eliasdb <elias.db3@gmail.com>
2019-09-20 13:44:06 +0200

    styling doc + functionality

---
+++ Chapter 4/The Monster Slayer/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false // to decide whether to show the start new game button or the attack/heal buttons
+    }
+});
