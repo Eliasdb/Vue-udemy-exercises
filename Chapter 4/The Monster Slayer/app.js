b8191463d4971a3b38d831d7bf8245bcd1e2fc73
Eliasdb <elias.db3@gmail.com>
2019-09-23 12:26:15 +0200

    refactoring

--- Chapter 4/The Monster Slayer/app.js
+++ Chapter 4/The Monster Slayer/app.js
@@ -11,17 +11,18 @@ new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
-        attack: function() {
-            var max = 10;
-            var min = 3;
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.monsterHealth -= damage;
 
-            max = 12;
-            min = 5;
-            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.playerHealth -= damage;
+        attack: function() {
+            this.monsterHealth -= this.calculateDamage(3, 10); // random number 3-10 to monster health
+
+            if (this.checkWin()) { //checking if we should continue function or not, will check if this.checkWin returns true which is always the case if we either lost or won. if we return true here, I know I don't want to continue the code execution here because the game is over, I don't want to deal damage again. We start a new game or did nothing.
+                return; //stops executing code
+            }
+
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin(); //end of functions, doesn't need return
         },
+
         specialAttack: function() {
 
         },
@@ -30,6 +31,29 @@ new Vue({
         },
         giveUp: function() {
 
+        },
+        calculateDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min); // see own documentation for explanation
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) { // alerts 'you won'
+                if (confirm('You won! New game?')) { //asks to play again
+                    this.startGame(); // restarts the game if yes
+                } else {
+                    this.gameIsRunning = false; // if the player says no, it doesn't start a new game
+                }
+                return true;
+
+
+            } else if (this.playerHealth <= 0) { //alerts 'you lost'
+                if (confirm('You lost! New game?')) { //asks to play again
+                    this.startGame(); //restarts the game if yes
+                } else {
+                    this.gameIsRunning = false; // if the player says no, it doesn't start a new game
+                }
+                return true;
+            }
+            return false; //only if we satisfy no condition
         }
     }
 });
